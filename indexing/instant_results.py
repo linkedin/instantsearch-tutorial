a5ba85f42accfebf3a2447011124ed44e6f3e2e5
Abhi Lad <alad@linkedin.com>
2016-07-08 15:41:18 -0700

    Cleaned up instant results script
    * Use cat and head to read data
    * Updated README with appropriate instructions

--- indexing/instant_results.py
+++ indexing/instant_results.py
@@ -4,7 +4,7 @@ https://www.elastic.co/guide/en/elasticsearch/guide/current/_index_time_search_a
 """
 from __future__ import unicode_literals
 
-import argparse
+import sys
 import json
 import requests
 
@@ -67,38 +67,27 @@ def create_index():
     requests.put(url=url, data=json.dumps(index_mapping))
 
 
-def run(input_file, max_lines):
-    create_index()
-    # samples
-    url = LOCAL_HOST + '/' + INDEX_NAME + '/' + FIELD_NAME
-    with open(input_file, 'rb') as f_input:
-        for line_number, line in enumerate(f_input):
-            if (line_number % 1000) == 0:
-                print 'Done with indexing: %d posts' % line_number
-            if max_lines and (line_number >= max_lines):
-                break
-            current_entry = json.loads(line)
-            current_title = current_entry.get(TITLE_KEY)
-            if not current_title:
-                continue
-            post_id = current_entry.get('@Id', -1)
-            new_url = url + '/' + post_id
-            popularity = current_entry.get('@Score', 0)
-            data = {
-                'name': current_title,
-                'popularity': float(popularity),
-            }
-            requests.put(url=new_url, data=json.dumps(data))
-        print 'INDEXED: %d documents' % line_number
-
-
 def main():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--i', action='store', dest='input', )
-    arg_parser.add_argument('--m', action='store', dest='max_lines', type=int)
-    args = arg_parser.parse_args()
-    run(input_file=args.input, max_lines=args.max_lines)
-
+    create_index()
+    url = LOCAL_HOST + '/' + INDEX_NAME + '/' + FIELD_NAME
+    line_number = 0
+    for line in sys.stdin:
+        line_number += 1
+        if (line_number % 1000) == 0:
+            print 'Done with indexing %d posts' % line_number
+        current_entry = json.loads(line)
+        current_title = current_entry.get(TITLE_KEY)
+        if not current_title:
+            continue
+        post_id = current_entry.get('@Id', -1)
+        new_url = url + '/' + post_id
+        popularity = current_entry.get('@Score', 0)
+        data = {
+            'name': current_title,
+            'popularity': float(popularity),
+        }
+        requests.put(url=new_url, data=json.dumps(data))
+    print 'INDEXED: %d documents' % line_number
 
 if __name__ == '__main__':
     main()

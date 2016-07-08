7e2b2ff1ce76784cf98b6f7dd03e1beb6defaf64
Abhi Lad <alad@linkedin.com>
2016-07-08 15:03:22 -0700

    Removed utils folder

--- utils/simple_curl_suggest_request.sh
+++
@@ -1,8 +0,0 @@
-curl -X POST 'localhost:9200/stackoverflowtags/_suggest?pretty' -d '{
-    "tag-suggest" : {
-        "text" : "n",
-            "completion" : {
-                    "field" : "suggest"
-                        }
-                        }
-}'''
